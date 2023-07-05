20a25106151f950ef020e4de4599b8268542606e
Christo Buschek <christo.buschek@proton.me>
2023-07-05 16:51:09 +0200

    feat: convert docs to pdfs

--- redact_pdfs/redact.py
+++ redact_pdfs/redact.py
@@ -3,6 +3,19 @@ import fitz
 import re
 import click
 import sys
+import os
+import pathlib
+import subprocess
+import tempfile
+
+
+TIMEOUT = 3600  # seconds
+CONVERT_RETRIES = 5
+
+
+class ProcessingException(Exception):
+    "A data-related error occuring during file processing."
+    pass
 
 
 class Redactor:
@@ -56,6 +69,63 @@ class Redactor:
             logging.info(f"{self.input} had no redactions.")
 
 
+def document_to_pdf(unique_tmpdir, file_path, timeout=TIMEOUT):
+    """Converts an office document to PDF."""
+    logging.info("Converting [%s] to PDF", file_path)
+
+    pdf_output_dir = os.path.join(unique_tmpdir, "out")
+    libreoffice_profile_dir = os.path.join(unique_tmpdir, "profile")
+    pathlib.Path(pdf_output_dir).mkdir(parents=True)
+    pathlib.Path(libreoffice_profile_dir).mkdir(parents=True)
+
+    cmd = [
+        # "/usr/bin/libreoffice",
+        "/opt/homebrew/bin/soffice",
+        '"-env:UserInstallation=file://{}"'.format(libreoffice_profile_dir),
+        "--nologo",
+        "--headless",
+        "--nocrashreport",
+        "--nodefault",
+        "--norestore",
+        "--nolockcheck",
+        "--invisible",
+        "--convert-to",
+        "pdf",
+        "--outdir",
+        pdf_output_dir,
+        file_path,
+    ]
+
+    try:
+        for attempt in range(1, CONVERT_RETRIES):
+            logging.info(
+                f"Starting LibreOffice: %s with timeout %s attempt #{attempt}/{CONVERT_RETRIES}",
+                cmd,
+                timeout,
+            )
+            try:
+                subprocess.run(cmd, timeout=timeout, check=True)
+            except Exception as e:
+                logging.info(
+                    f"Could not be converted to PDF (attempt {attempt}/{CONVERT_RETRIES}): {e}"
+                )
+                continue
+
+            for file_name in os.listdir(pdf_output_dir):
+                if not file_name.endswith(".pdf"):
+                    continue
+                out_file = os.path.join(pdf_output_dir, file_name)
+                if os.stat(out_file).st_size == 0:
+                    continue
+                logging.info(f"Successfully converted {out_file}")
+                return out_file
+        raise ProcessingException(
+            f"Could not be converted to PDF (attempt #{attempt}/{CONVERT_RETRIES})"
+        )
+    except Exception as e:
+        raise ProcessingException("Could not be converted to PDF") from e
+
+
 @click.command()
 @click.option("--redactions", type=click.File("r"), help="List of terms to redact.")
 @click.option("--input", type=click.Path(exists=True), help="The file to redact.")
@@ -81,12 +151,16 @@ def main(redactions, input, output, log_file):
     for line in redactions.readlines():
         redactions_re.append(line.strip())
 
-    redactor = Redactor(
-        input,
-        output,
-    )
+    with tempfile.TemporaryDirectory() as unique_tmpdir:
+        # TODO - write to logs the case in which the context manager can't delete these dirs
+        pdf_path = document_to_pdf(unique_tmpdir, input)
 
-    redactor.redaction(redactions_re)
+        redactor = Redactor(
+            pdf_path,
+            output,
+        )
+
+        redactor.redaction(redactions_re)
 
 
 if __name__ == "__main__":
